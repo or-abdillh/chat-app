d461c8b27c102a450ffd4cecd9857a81aa2c9d83
or abdillh <hans.abdillh05@gmail.com>
2022-02-15 00:36:18 +0800

    Change ChatAction

--- src/components/ChatAction.vue
+++ src/components/ChatAction.vue
@@ -1,6 +1,6 @@
 <template>
-	<main class="fixed bottom-0 left-0 right-0 py-2">
-		<section class="bg-primary w-full md:px-28 lg:p-5 lg:w-4/12 md:mx-auto xl:w-3/12 px-5 flex justify-between">
+	<main class="fixed bottom-0 left-0 right-0">
+		<section class="bg-primary p-5 w-full md:px-28  lg:p-5 lg:w-4/12 md:mx-auto xl:w-3/12 px-5 flex justify-between">
 			<div class="w-10/12 text-gray-100 bg-secondary px-3 py-1 flex items-center rounded-xl">
 				<i class="active:scale-90 duration-300 fa fa-link mr-3 text-sm"></i>
 				<input @keyup.enter="newChat" v-model="textField" type="text" placeholder="Your message" class="bg-secondary w-full px-2 py-1 rounded-xl" />
@@ -8,7 +8,7 @@
 			<span>
 			<i class="active:scale-90 duration-300 fa fa-microphone text-indigo-600 text-3xl"></i>
 			</span>
-		</section>>
+		</section>
 	</main>
 </template>
 
